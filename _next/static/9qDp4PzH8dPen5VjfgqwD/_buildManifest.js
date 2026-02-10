23912434361da83e9fda4f0074d69e3ced4f287e
Jabbar Ahamed Jaheeth <jaheeth.offi0@gmail.com>
2026-02-11 01:04:42 +0530

    Updates

--- _next/static/9qDp4PzH8dPen5VjfgqwD/_buildManifest.js
+++ _next/static/9qDp4PzH8dPen5VjfgqwD/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/Jaheeth_portfolio//_next/:path+",
-        "destination": "/Jaheeth_portfolio/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
